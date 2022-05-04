862474f70908e4f023a9ff2949da814b783feb0e
Alex Warlockes <kirushckin.alexei@gmail.com>
2022-05-04 15:43:15 +0300

    Создание пользователя

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,18 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { IsEmail } from 'class-validator';
+
+@Entity('users')
+export class UserEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  fullName: string;
+
+  @Column()
+  @IsEmail()
+  email: string;
+
+  @Column({ nullable: true })
+  password?: string;
+}
